132c8b6507e244a1c9197770d3a60a89f25adefe
Hunternif <hunternif@gmail.com>
2022-05-14 16:55:45 +0100

    Add logging

--- cheems_bot/config.py
+++ cheems_bot/config.py
@@ -1,10 +1,28 @@
+import logging
+import sys
+
 import yaml
 
+# Config variables
 discord_token = ''
 
-with open('config.yaml', 'r') as f:
-    try:
+
+# Logger config
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
+# Local logger
+logger = logging.getLogger(__name__)
+
+try:
+    with open('config.yaml', 'r') as f:
         config = yaml.safe_load(f)
         discord_token = config['discord_token']
-    except Exception as e:
-        print(f"Couldn't read config.yaml. Caught {e}")
+except Exception:
+    logger.exception("Couldn't read config.yaml")
--- cheems_bot/main.py
+++ cheems_bot/main.py
@@ -1,14 +1,20 @@
+import logging
+
 import config
 from discord.ext import commands
 
 # Initialize Bot and Denote The Command Prefix
 bot = commands.Bot(command_prefix="!")
+logger = logging.getLogger('cheems_bot')
 
 
 # Runs when Bot Successfully Connects
 @bot.event
 async def on_ready():
-    print(f'{bot.user} successfully logged in!')
+    logger.info(f'{bot.user} successfully logged in!')
 
 
-bot.run(config.discord_token)
+try:
+    bot.run(config.discord_token)
+except Exception:
+    logger.exception("Couldn't run bot")
